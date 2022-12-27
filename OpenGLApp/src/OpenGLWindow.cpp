d3a80d86494112c0ca408c53ea50da20f0d69efe
etavardt <etavardt@gmail.com>
2022-12-27 12:00:52 -0600

    Move Shader code to external Files.

--- OpenGLApp/src/OpenGLWindow.cpp
+++ OpenGLApp/src/OpenGLWindow.cpp
@@ -1,5 +1,44 @@
 #include <GL\glew.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "OpenGLWindow.hpp"
+
+struct ShaderProgramSource {
+    std::string vertexSource;
+    std::string fragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath) {
+    std::ifstream stream(filepath);
+    std::stringstream ss[2];
+
+    enum class ShaderType {
+        NONE = -1,
+        VERTEX = 0,
+        FRAGMENT = 1
+    };
+
+    ShaderType type = ShaderType::NONE;
+
+    std::string line;
+    while (getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::VERTEX;
+            } else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::FRAGMENT;
+            }
+        } else {
+            ss[(int)type] << line << "\n";
+        }
+    }
+    //ShaderProgramSource sps;
+    //sps.vertexSource = ss[0].str();
+    //sps.fragmentSource = ss[1].str();
+    return { ss[0].str(), ss[1].str() };
+}
 
 static int CompileShader(GLuint type, const std::string& source) {
     //std::assert(source);
@@ -100,28 +139,16 @@ void OpenGLWindow::init() {
     //glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(triVertexBuf)/3, 0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2 , 0);
 
-    std::string vertexShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = position;\n"
-        "}\n";
+    //ShaderProgramSource source = ParseShader("res/shaders/basicRedTri.shader");
+    ShaderProgramSource source = ParseShader("res/shaders/basicBlueTri.shader");
 
-    std::string fragmentShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) out vec4 color;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "    color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n";
+    std::cout << "VERTEX SHADER" << std::endl;
+    std::cout << source.vertexSource << std::endl;
+    std::cout << "FRAGMENT SHADER" << std::endl;
+    std::cout << source.fragmentSource << std::endl;
 
     //GLuint shader = CreateShader(vertexShader, fragmentShader);
-    shader = CreateShader(vertexShader, fragmentShader);
+    shader = CreateShader(source.vertexSource, source.fragmentSource);
     glUseProgram(shader);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
